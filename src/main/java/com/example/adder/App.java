fbe5aa824010e70bf9c076929ba1f9bd53c38be9
praphul <praphulsingh1712@gmail.com>
2025-09-12 11:18:25 +0530

    maven

---
+++ src/main/java/com/example/adder/App.java
@@ -0,0 +1,13 @@
+package com.example.adder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("Sum is: " + (a + b));
+    }
+}
